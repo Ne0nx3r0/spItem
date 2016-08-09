bd7848bcc9cb2257ddd5dacd8ad69a2d4a97bcc4
Ne0nx3r0 <ne0nx3r0@gmail.com>
2016-08-09 16:25:03 -0500

    Implementing getValuePairs

--- spDataService.js
+++ spDataService.js
@@ -3,15 +3,25 @@ const jQuery = require('./lib/jquery-1.11.3.js');
 const SPServices = require('./lib/jquery.SPServices.js');
 
 class spDataService{
+    create(options){
+        let o = $.extend({
+            model:undefined,//required
+        },options);
+
+        let d = $.Deferred();
+
+        let valuePairs = this._getValuePairs();
+
+        
+
+        return d.promise();
+    }
+
     getAll(options){
 
     }
 
     getById(options){
-
-    }
-
-    create(options){
 
     }
 
@@ -30,4 +40,19 @@ class spDataService{
     delete(options){
 
     }
+
+    _getValuePairs(spModel){
+        let valuePairs = [];
+
+        spModel.keySeq().forEach(function(key){
+            let val = spModel.get(key);
+
+            if(val){
+                valuePairs.push([key,val]);
+            }
+        });
+
+        return valuePairs;
+    }
 }
+
