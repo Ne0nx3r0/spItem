c0c7ea48a5b40988320967e5727b1edeead01be8
Ne0nx3r0 <ne0nx3r0@gmail.com>
2016-08-09 16:29:57 -0500

    Implementing spd.create

--- spDataService.js
+++ spDataService.js
@@ -3,16 +3,37 @@ const jQuery = require('./lib/jquery-1.11.3.js');
 const SPServices = require('./lib/jquery.SPServices.js');
 
 class spDataService{
-    create(options){
-        let o = $.extend({
-            model:undefined,//required
-        },options);
+    //create({model: new spItem()})
+    create(o){
+        let spModel = o.model;
 
         let d = $.Deferred();
 
-        let valuePairs = this._getValuePairs();
+        $().SPServices({
+			operation: 'UpdateListItems',
+			listName: spModel.getListName(),
+			webURL: spModel.getSiteUrl(),
+			batchCmd: 'New',
+			debug:true,// <- Show us the errorsvaluepairs: this._getValuePairs(spModel),
+			completefunc: function(xData,status){
+				let $xml = $(xData.responseXML);
+				let errorCode = $xml.find('ErrorCode').text();
 
-        
+				if(errorCode == '0x00000000'){
+					d.resolve({
+						// Because the schema and potentially necessary fields aren't
+						// handed back with this call we force 
+						// the caller to make their own call if they want more data
+						modelId: $xml.find('z\\:row').attr('ows_ID')
+					});
+				}
+				else{
+					d.reject({
+						error: 'Error: '+$xml.find('ErrorText').text()
+					});
+				}   
+			}
+		});
 
         return d.promise();
     }
